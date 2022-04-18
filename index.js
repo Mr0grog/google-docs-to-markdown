fe62020ea5846acbde9901e8fe27f4a481d01fd7
Rob Brackett <rob@robbrackett.com>
2022-04-17 19:17:44 -0700

    Support superscript/subscript, fixes #33

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 import fixGoogleHtml from './lib/fix-google-html';
 // rehype-dom-parse is a lightweight version of rehype-parse that leverages
 // browser APIs -- reduces bundle size by ~200 kB!
-// const parse = require('rehype-dom-parse').default;
 import parse from 'rehype-dom-parse';
+import { all } from 'rehype-remark';
 import rehype2remarkWithSpaces from './lib/rehype-to-remark-with-spaces';
 import stringify from 'remark-stringify';
 import { unified } from 'unified';
 
 
+function preserveTagAndConvertContents (h, node) {
+  return [
+    h(node, 'html', `<${node.tagName}>`),
+    ...all(h, node),
+    h(node, 'html', `</${node.tagName}>`)
+  ];
+}
+
 const processor = unified()
   .use(parse)
   .use(fixGoogleHtml)
   // .use(require('./lib/log-tree').default)
-  .use(rehype2remarkWithSpaces)
+  .use(rehype2remarkWithSpaces, {
+    handlers: {
+      // Preserve sup/sub markup; most Markdowns have no markup for it.
+      sub: preserveTagAndConvertContents,
+      sup: preserveTagAndConvertContents
+    }
+  })
   .use(stringify, {listItemIndent: '1'});
 
 function convertToMarkdown (html) {
