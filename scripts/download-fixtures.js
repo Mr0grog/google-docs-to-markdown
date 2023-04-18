d144838b58b738a6ab4e7d3dc083c112ca52533d
Rob Brackett <rob@robbrackett.com>
2023-04-18 15:25:13 -0700

    Add inline-formatting fixture

--- scripts/download-fixtures.js
+++ scripts/download-fixtures.js
@@ -10,6 +10,7 @@ const COMMAND_KEY = process.platform === 'darwin' ? 'Meta' : 'Control';
 const FIXTURE_PATH = '../test/fixtures';
 const FIXTURES = {
   'headings-and-paragraphs': '1Dm5DIHOVAvsGYgTaPuhq78PefT_5u10RFsRBxunxBtQ',
+  'inline-formatting': '1-0E8y62m1tI6MWYYbGcbBALylL9hT9Toq9SssCeT-Ew',
   'lists': '1bZI3NwaasFZGexGQG9YC07UAovpY9b_mfdI2_KgT8-0'
 };
 
