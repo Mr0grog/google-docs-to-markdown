525c46f76a98ed233d84d8feb052b81a36857d58
Rob Brackett <rob@robbrackett.com>
2023-04-18 17:22:32 -0700

    More stable format for exports
    I'm not sure this is really working that well, and it has a lot of complex code. Not sure I should keep it.

--- scripts/download-fixtures.js
+++ scripts/download-fixtures.js
@@ -100,6 +100,121 @@ async function getExportedGoogleDocHtml(documentId) {
   return await response.text();
 }
 
+/**
+ * Clean up copied HTML from a Google Doc so that identical docs have identical
+ * HTML.
+ * @param {string} html
+ * @returns {string}
+ */
+function cleanCopiedHtml(html) {
+  // Google Docs adds a unique GUID to every copy operation. Overwrite it
+  // so we only track meaningful changes to the content of the fixture.
+  return html.replace(
+    /id="docs-internal-guid-\w{8}-\w{4}-\w{4}-\w{4}-\w{12}"/,
+    `id="docs-internal-guid-dddddddd-dddd-dddd-dddd-123456789abc"`
+  );
+}
+
+/**
+ * Clean up exported HTML from a Google Doc so that identical docs have
+ * identical HTML.
+ * 
+ * The Docs exporter does some pretty funky stuff, with lots of CSS bits coming
+ * in an unpredictable order. This tries to rename and sort classes and rules
+ * so they are predictable, but sometimes Google Docs exports different sets
+ * of classes for the same content (e.g. sometimes common styles for multiple
+ * elements get separate classes and sometimes they don't for the exact same
+ * document!). I'm not sure we can do much about that, and it's probably not
+ * worth too much effort to fix for this script.
+ * @param {string} html
+ * @returns {string}
+ */
+function cleanExportedHtml(html) {
+  // Exported docs use a set of seemingly randomly-numbered class names.
+  // Re-number them in document order.
+  const oldToNewName = {};
+  let nextNumber = 1;
+
+  // Rename the classes in HTML `class` attributes.
+  let reformatted = html.replace(/\sclass="([^"]+)"(\s|>)/ig, (_, classValue, ending) => {
+    const newClasses = classValue
+      .split(" ")
+      .map((name) => {
+        // Only rename the names like "c1" (there are other meaningful names).
+        if (!/^c\d+$/.test(name)) return name;
+
+        let newName = oldToNewName[name];
+        if (!newName) {
+          newName = oldToNewName[name] = `c${nextNumber}`;
+          nextNumber++;
+        }
+        
+        return newName;
+      })
+      .join(' ');
+    return ` class="${newClasses}"${ending}`;
+  });
+
+  // Update the stylesheet to use the new class names and sort them.
+  try {
+    const numberedSelectorPattern = /\.(c\d+)\b/g;
+    
+    // TODO: this should probably use a CSS parser instead of all these crazy
+    // regexes to avoid weird cases.
+    reformatted = reformatted.replace(/(<style[^>]*>)(.*?)<\/style>/, (_, opening, cssText) => {
+      if (cssText.includes('@')) {
+        console.warn(
+          "It looks like there is an @-rule in the document's CSS, which we "
+          + "can't handle. Saving as-is without reformatting CSS."
+        );
+        throw Object.assign(new Error("Complex CSS"), { code: 'abort' });
+      }
+
+      // Pull out the CSS rules for renamed classes.
+      const rules = [];
+      const extraCss = cssText.replace(/([^\s@{][^{]*)\{(.*?)}/g, (text, selector, body) => {
+        if (selector.match(numberedSelectorPattern)) {
+          rules.push({ text, selector, body });
+          return '';
+        }
+        else {
+          return text;
+        }
+      });
+
+      // Rename the classes in the selectors and sort them.
+      const newCssText = rules
+        .map((rule) => {
+          numberedSelectorPattern.lastIndex = 0;
+          const newSelector = rule.selector.replace(numberedSelectorPattern, (match, oldName) => {
+            const newName = oldToNewName[oldName];
+            if (newName) {
+              return `.${newName}`;
+            }
+            else {
+              console.warn(`Found un-renamed CSS class in document stylesheet: "${oldName}". Leaving it as-is.`);
+              return match;
+            }
+          });
+          return { ...rule, selector: newSelector };
+        })
+        .sort((a, b) => (a.selector < b.selector ? -1 : 1))
+        .map((rule) => `${rule.selector}{${rule.body}}`)
+        .join('');
+
+      return `${opening}${extraCss}${newCssText}</style>`;
+    });
+  }
+  catch (error) {
+    if (error.code === 'abort') {
+      return html;
+    }
+    throw error;
+  }
+
+  return reformatted;
+}
+
 async function downloadFixtures(destination) {
   console.log(`Downloading fixtures to: "${destination}"`);
   // Firefox strips the rich formatting when pasting in headless mode (!), but
@@ -110,17 +225,13 @@ async function downloadFixtures(destination) {
       console.log(`Loading ${name} (Google doc: "${id}")...`);
 
       let copied = await getCopiedGoogleDocHtml(browser, id);
-      // Google Docs adds a unique GUID to every copy operation. Overwrite it
-      // so we only track meaningful changes to the content of the fixture.
-      copied = copied.replace(
-        /id="docs-internal-guid-\w{8}-\w{4}-\w{4}-\w{4}-\w{12}"/,
-        `id="docs-internal-guid-dddddddd-dddd-dddd-dddd-123456789abc"`
-      );
+      copied = cleanCopiedHtml(copied);
       await writeFile(path.join(destination, `${name}.copy.html`), copied, {
         encoding: 'utf-8'
       });
 
-      const exported = await getExportedGoogleDocHtml(id);
+      let exported = await getExportedGoogleDocHtml(id);
+      exported = cleanExportedHtml(exported);
       await writeFile(path.join(destination, `${name}.export.html`), exported, {
         encoding: 'utf-8'
       });
