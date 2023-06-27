d055b0fcac9bdfbb894f1dba9b3c18729517184d
Rob Brackett <rob@robbrackett.com>
2023-06-27 16:27:24 -0700

    Add more tests

--- test/unit/css.test.js
+++ test/unit/css.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'expect';
-import { parseCssPropertyList } from '../../lib/css.js';
+import { StyleResolver, parseCssPropertyList } from '../../lib/css.js';
 
 describe('parseCssPropertyList', () => {
   it('parses simple CSS property lists', () => {
@@ -39,3 +39,81 @@ describe('parseCssPropertyList', () => {
     expect(result).toEqual({ color: 'blue' });
   });
 });
+
+describe('StyleResolver', () => {
+  it('resolves inherited styles from ancestors', () => {
+    const style = new StyleResolver(
+      { properties: { style: 'color: blue;' } },
+      [
+        { properties: { style: 'font-weight: 700;' } },
+        { properties: { style: 'font-style: italic;' } }
+      ]
+    );
+    expect(style.get('color')).toBe('blue');
+    expect(style.get('font-weight')).toBe('700');
+    expect(style.get('font-style')).toBe('italic');
+  });
+
+  it('resolves to the explicitly set value if there is one', () => {
+    const style = new StyleResolver(
+      { properties: { style: 'font-weight: 600;' } },
+      [
+        { properties: { style: 'font-weight: 700;' } },
+        { properties: { style: 'font-weight: 400;' } }
+      ]
+    );
+    expect(style.get('font-weight')).toBe('600');
+  });
+
+  it('resolves to the closest value from an ancestor if not set explicitly', () => {
+    const style = new StyleResolver(
+      { properties: { style: 'color: blue;' } },
+      [
+        { properties: { style: 'font-weight: 700;' } },
+        { properties: { style: 'font-weight: 400;' } }
+      ]
+    );
+    expect(style.get('font-weight')).toBe('400');
+  });
+
+  it('works if the node has no style', () => {
+    const style = new StyleResolver(
+      {},
+      [
+        { properties: { style: 'font-weight: 700;' } },
+        { properties: { style: 'font-style: italic;' } }
+      ]
+    );
+    expect(style.get('font-weight')).toBe('700');
+    expect(style.get('font-style')).toBe('italic');
+  });
+
+  it('works if an ancestor has no style', () => {
+    const style = new StyleResolver(
+      { properties: { style: 'color: blue;' } },
+      [
+        {},
+        { properties: { style: 'font-style: italic;' } }
+      ]
+    );
+    expect(style.get('color')).toBe('blue');
+    expect(style.get('font-style')).toBe('italic');
+  });
+
+  it('works if there are no ancestors', () => {
+    const style = new StyleResolver({ properties: { style: 'color: blue;' } });
+    expect(style.get('color')).toBe('blue');
+    expect(style.get('font-style')).toBe(undefined);
+  });
+
+  it('resolves to a parent value when set to "inherit"', () => {
+    const style = new StyleResolver(
+      { properties: { style: 'color: inherit;' } },
+      [
+        { properties: { style: 'color: red;' } },
+        { properties: { style: 'color: yellow;' } }
+      ]
+    );
+    expect(style.get('color')).toBe('yellow');
+  });
+})
