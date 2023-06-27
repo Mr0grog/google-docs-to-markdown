6ae5e257c811e440cee209c0a0d060bdda1dc056
Rob Brackett <rob@robbrackett.com>
2023-06-27 13:19:35 -0700

    Move CSS handling into its own module
    This keeps things a bit more organized, especially since this will likely get more complex when handling HTML *exported* (instead of copied from) Google Docs.

---
+++ test/unit/css.test.js
@@ -0,0 +1,36 @@
+import { expect } from 'expect';
+import { parseCssPropertyList } from '../../lib/css.js';
+
+describe('parseCssPropertyList', () => {
+  it('parses simple CSS property lists', () => {
+    const result = parseCssPropertyList(`
+      color: blue;
+      margin: 5em;
+      font-family: helvetica, arial, sans-serif;
+      -webkit-line-clamp: none;
+    `);
+
+    expect(result).toEqual({
+      color: 'blue',
+      margin: '5em',
+      'font-family': 'helvetica, arial, sans-serif',
+      '-webkit-line-clamp': 'none'
+    });
+  });
+
+  it('handles empty properties', () => {
+    const result = parseCssPropertyList(`color: blue;; ; margin: 5em;`);
+    expect(result).toEqual({
+      color: 'blue',
+      margin: '5em'
+    });
+  });
+
+  it('uses the last specified value of a property', () => {
+    const result = parseCssPropertyList(`color: blue;margin: 5em;color: red;`);
+    expect(result).toEqual({
+      color: 'red',
+      margin: '5em'
+    });
+  })
+});
