51c9f1207633d4103f17e524e8fc95352219a300
Rob Brackett <rob@robbrackett.com>
2023-07-27 18:12:51 -0700

    Move test prepare logic to base configuration

--- wdio.conf.base.js
+++ wdio.conf.base.js
@@ -1,3 +1,4 @@
+import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { getTestTempDirectory } from './test/support/utils.js';
@@ -8,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 global.tempDirectory = path.join(__dirname, 'temp');
 
+// Configuration for all testable browsers.
 let capabilities = [
   {
     browserName: 'chrome',
@@ -105,4 +107,18 @@ export const config = {
     ui: 'bdd',
     timeout: 60_000
   },
+
+  async onPrepare (_config, capabilities) {
+    // Ensure we have a clean temp directory.
+    await fs.rm(global.tempDirectory, { recursive: true, force: true });
+    await fs.mkdir(global.tempDirectory);
+
+    // And a subdirectory for each test environment -- they run in parallel,
+    // so need their own isolated space to play in.
+    for (const capability of capabilities) {
+      const browserDirectory = getTestTempDirectory(capability);
+      await fs.rm(browserDirectory, { recursive: true, force: true });
+      await fs.mkdir(browserDirectory);
+    }
+  }
 };
--- wdio.conf.e2e.js
+++ wdio.conf.e2e.js
@@ -1,7 +1,5 @@
-import * as fs from 'node:fs/promises';
 import { config as base } from './wdio.conf.base.js';
 import WebpackDevServerService from './test/support/wdio-webpack-dev-server.js';
-import { getTestTempDirectory } from './test/support/utils.js';
 
 export const config = {
   ...base,
@@ -13,19 +11,5 @@ export const config = {
   services: [
     ...base.services,
     [WebpackDevServerService, {}]
-  ],
-  // FIXME: move to base configuration, use an async function and fs/promises API
-  async onPrepare (_config, capabilities) {
-    // Ensure we have a clean temp directory.
-    await fs.rm(global.tempDirectory, { recursive: true, force: true });
-    await fs.mkdir(global.tempDirectory);
-
-    // And a subdirectory for each test environment -- they run in parallel,
-    // so need their own isolated space to play in.
-    for (const capability of capabilities) {
-      const browserDirectory = getTestTempDirectory(capability);
-      await fs.rm(browserDirectory, { recursive: true, force: true });
-      await fs.mkdir(browserDirectory);
-    }
-  }
+  ]
 };
