99fa8f4b229cecc6219147ddc70d497e472c99fb
Michael Bianco <iloveitaly@gmail.com>
2023-10-29 17:10:07 -0600

    fix: use simplier stdin read

--- cmd.js
+++ cmd.js
@@ -1,47 +1,13 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
 import {convertDocsHtmlToMarkdown} from './lib/convert.js';
-
-// to avoid top-level async/await. readFileSync(0) caused strange errors
-// https://stackoverflow.com/questions/67037353/any-way-to-read-from-stdin-synchronously-without-extra-modules-libraries-node-j
-function stdinReadSync() {
-    if (process.stdin.isTTY) {
-      return '';
-    }
-
-    const b = Buffer.alloc(1024)
-    let data = ''
-
-    while (true) {
-        let n = 0
-
-        // Read while EAGAIN
-        while (true) {
-            try {
-                n = fs.readSync(process.stdin.fd, b, 0, b.length)
-                break
-            } catch (e) {
-                if (e.code === 'EAGAIN') {
-                    // Sleep 100ms
-                    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 100)
-                    continue
-                }
-                throw e
-            }
-        }
-
-        if (!n) break
-        data += b.toString('utf8', 0, n)
-    }
-
-    return data
-}
+import { buffer as readBuffer } from 'node:stream/consumers';
 
 // in order to debug this tool over the command line, you can read a file with broken input locally
 // ex: fs.readFileSync('./local-file.html', 'utf8');
 
-const inputHTML = stdinReadSync()
+const rawInput = await readBuffer(process.stdin);
+const inputHTML = rawInput.toString('utf-8');
 
 if(!inputHTML) {
   console.error('no HTML provided over stdin');
