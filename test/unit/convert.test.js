26208491d3b291f19aca971f62be6f003bb8770e
Rob Brackett <rob@robbrackett.com>
2023-11-28 21:32:54 -0800

    Include GDocs Slice Clip data in copy/paste tests
    It's good to actually test the code! Especially since the new code does not pass its test now that we are actually running it. 😬

--- test/unit/convert.test.js
+++ test/unit/convert.test.js
@@ -8,8 +8,12 @@ describe('convert', () => {
 
     test(`converts ${name} (${type})`, async () => {
       const input = await loadFixture(`${name}.${type}.html`);
+      const inputSliceClip = type === 'copy'
+        ? await loadFixture(`${name}.${type}.gdocsliceclip.json`)
+        : null;
       const expected = await loadFixture(`${name}.expected.md`);
-      const md = await convertDocsHtmlToMarkdown(input);
+
+      const md = await convertDocsHtmlToMarkdown(input, inputSliceClip);
       expect(md).toEqual(expected);
     });
   }
@@ -42,9 +46,8 @@ describe('convert', () => {
   createFixtureTest('tables', { type: 'copy' });
   createFixtureTest('tables', { type: 'export', skip: true });
 
-  // TODO: utilize internal clipboard data in tests, then enable these tests.
-  // createFixtureTest('internal-links', { type: 'copy' });
-  // createFixtureTest('internal-links', { type: 'export', skip: true });
+  createFixtureTest('internal-links', { type: 'copy' });
+  createFixtureTest('internal-links', { type: 'export', skip: true });
 
   // At current, it doesn't seem like this situation can happen in a Google Doc,
   // but it's worth supporting just in case things change or users want to
