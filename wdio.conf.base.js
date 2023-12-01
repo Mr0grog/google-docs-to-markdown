de75599df073068be7723ee30102efbc4a1d7f3b
Rob Brackett <rob@robbrackett.com>
2023-12-01 08:54:22 -0800

    Test possible misconfiguration in CI
    CI has been pretty flaky lately, and while that is probably the result of some changes in WebdriverIO, I noticed that I think we are not checking for whether we are running on CI correctly!
    
    This just adds some logging to see what's happening in CI so I can figure out what might be changed to improve things.

--- wdio.conf.base.js
+++ wdio.conf.base.js
@@ -4,6 +4,8 @@ import { fileURLToPath } from 'node:url';
 import { getTestTempDirectory } from './test/support/utils.js';
 
 const IS_CI = /^(true|1)$/i.test(process.env.ci?.trim() || '');
+console.warn(`TESTING CI CHECKS... IS_CI=${IS_CI}`);
+console.warn(`TESTING CI CHECKS... Uppercase=${/^(true|1)$/i.test(process.env.CI?.trim() || '');}`);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
