a70887a72f2f9c299a44e03bba343d677123ff9e
Rob Brackett <rob@robbrackett.com>
2024-08-13 17:16:54 -0700

    Add failing tests for suggestion changes handling

--- test/unit/convert.test.js
+++ test/unit/convert.test.js
@@ -50,6 +50,9 @@ describe('convert', () => {
   createFixtureTest('internal-links', { type: 'copy' });
   createFixtureTest('internal-links', { type: 'export', skip: true });
 
+  createFixtureTest('suggestions', { type: 'copy' });
+  createFixtureTest('suggestions', { type: 'export', skip: true });
+
   // At current, it doesn't seem like this situation can happen in a Google Doc,
   // but it's worth supporting just in case things change or users want to
   // convert more arbitrary HTML.
