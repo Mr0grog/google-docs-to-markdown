7b7977d06093f86eeae216a20215985e4c591e98
Rob Brackett <rob@robbrackett.com>
2024-08-13 21:30:23 -0700

    Make test more reliable in Safari

--- wdio.conf.e2e.js
+++ wdio.conf.e2e.js
@@ -4,6 +4,11 @@ import WebpackDevServerService from './test/support/wdio-webpack-dev-server.js';
 export const config = {
   ...base,
   runner: 'local',
+
+  // Safari seems to have particular issues with automating user interaction in
+  // parallel with other browsers, so only run one browser at a time for e2e.
+  maxInstances: 1,
+
   specs: ['./test/e2e/**/*.test.js'],
   baseUrl: WebpackDevServerService.getServerUrl(),
   services: [...base.services, [WebpackDevServerService, {}]],
