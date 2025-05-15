c8272650ef724cd8623970a3bdb25058aaa5a83b
Rob Brackett <rob@robbrackett.com>
2025-05-15 18:07:39 +0100

    Use new `defineConfig` feature in wdio (#296)

--- wdio.conf.unit.js
+++ wdio.conf.unit.js
@@ -1,7 +1,8 @@
+import { defineConfig } from '@wdio/config';
 import { config as base } from './wdio.conf.base.js';
 
-export const config = {
+export const config = defineConfig({
   ...base,
   runner: 'browser',
   specs: ['./test/unit/**/*.test.js'],
-};
+});
