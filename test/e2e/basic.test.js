aab8ed794b58511a9f26b4b20ed236be1c065725
Rob Brackett <rob@robbrackett.com>
2026-05-26 14:02:49 -0700

    Try again because what even

--- test/e2e/basic.test.js
+++ test/e2e/basic.test.js
@@ -47,9 +47,9 @@ describe('Basic functionality', () => {
     const clickable = await $input.isClickable();
     console.warn(`INPUT IS CLICKABLE? ${clickable}`);
 
-    // await $input.execute((node) => {
-    //   node.focus();
-    // });
+    await $input.execute((node) => {
+      node.focus();
+    });
 
     await $input.click();
     // Ideally, this would be `browser.keys([Key.Ctrl, 'b'])`, but only some
