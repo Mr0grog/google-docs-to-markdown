9837653d27f1beea5bacb8338c845a4d4f531391
Rob Brackett <rob@robbrackett.com>
2026-05-26 12:57:37 -0700

    Disable extra tests, add more logging

--- wdio.conf.e2e.js
+++ wdio.conf.e2e.js
@@ -5,6 +5,7 @@ import WebpackDevServerService from './test/support/wdio-webpack-dev-server.js';
 export const config = defineConfig({
   ...base,
   runner: 'local',
+  logLevel: 'debug',
 
   // Safari seems to have particular issues with automating user interaction in
   // parallel with other browsers, so only run one browser at a time for e2e.
